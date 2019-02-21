2505fa70a35ab79b2d01b10d2f4df28f9cca35f0
otopba <otopba@inbox.ru>
2019-02-21 13:47:55 +0300

    Init

---
+++ app/src/main/java/ru/ok/technopolis/students/Student.java
@@ -0,0 +1,48 @@
+package ru.ok.technopolis.students;
+
+public class Student {
+
+    private String firstName;
+    private String secondName;
+    private boolean maleGender;
+    private int photo;
+
+    public Student(String firstName, String secondName, boolean maleGender, int photo) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.maleGender = maleGender;
+        this.photo = photo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public boolean isMaleGender() {
+        return maleGender;
+    }
+
+    public void setMaleGender(boolean maleGender) {
+        this.maleGender = maleGender;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
