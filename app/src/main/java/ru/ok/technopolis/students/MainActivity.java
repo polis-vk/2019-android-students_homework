23c99a46f200eae4edd578426408cb0fa1f607e7
Alex <korneev.ry@yandex.ru>
2019-02-28 01:16:48 +0300

    почти готово. осталось закодить фото и сделать немного доработок

--- app/src/main/java/ru/ok/technopolis/students/MainActivity.java
+++ app/src/main/java/ru/ok/technopolis/students/MainActivity.java
@@ -2,21 +2,107 @@ package ru.ok.technopolis.students;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StudentsRecyclerAdapter.OnActionInRecyclerViewListener {
+
+    private StudentsRecyclerAdapter studentsRecyclerAdapter;
+
+    private EditText edtFirstName, edtSecondName;
+    private RadioGroup radioGroup;
+
+    //TODO поработать со стилем
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_main);
 
+        setupRecyclerView();
 
+        edtFirstName = findViewById(R.id.activity_main__first_name);
+        edtSecondName = findViewById(R.id.activity_main__second_name);
+        radioGroup = findViewById(R.id.activity_main__gender);
+
+        Button btnSave = findViewById(R.id.activity_main__btn_save);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = edtFirstName.getText().toString(); //TODO проверка на пустоту данных
+                String secondName = edtSecondName.getText().toString(); //TODO проверка на пустоту данных
+
+                boolean gender;
+                switch (radioGroup.getCheckedRadioButtonId()) {
+                    case R.id.activity_main__gender_male:
+                        gender = true;
+                        break;
+                    case R.id.activity_main__gender_female:
+                        gender = false;
+                        break;
+                    default:
+                        return; //TODO проверка на пустоту данных, сообщение ошибки добавить
+                        //break;
+                }
+
+                Student student = new Student(firstName, secondName, gender, 0);
+                studentsRecyclerAdapter.saveStudent(student);
+            }
+        });
+
+        Button btnDelete = findViewById(R.id.activity_main__btn_delete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                studentsRecyclerAdapter.deleteStudent();
+            }
+        });
+
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.activity_main__recycler);
+        studentsRecyclerAdapter = new StudentsRecyclerAdapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(studentsRecyclerAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.top_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu__add){
+            onClearFields();
+            studentsRecyclerAdapter.setActionStudent(StudentsRecyclerAdapter.NO_ACTION_STUDENT);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClearFields() {
+        edtFirstName.setText("");
+        edtSecondName.setText("");
+        radioGroup.clearCheck();
+    }
+
+    @Override
+    public void onActionStudent(String fName, String sName, boolean gender, int photo) {
+        edtFirstName.setText(fName);
+        edtSecondName.setText(sName);
+        if (gender){
+            radioGroup.check(R.id.activity_main__gender_male);
+        } else {
+            radioGroup.check(R.id.activity_main__gender_female);
+        }
     }
 }
--- app/src/main/java/ru/ok/technopolis/students/StudentsRecyclerAdapter.java
+++ app/src/main/java/ru/ok/technopolis/students/StudentsRecyclerAdapter.java
@@ -1,5 +1,6 @@
 package ru.ok.technopolis.students;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,11 +9,60 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentsRecyclerAdapter extends RecyclerView.Adapter<StudentsRecyclerAdapter.StudentViewHolder> {
 
-    private List<Student> studentList;
+    public interface OnActionInRecyclerViewListener {
+        void onClearFields();
+        void onActionStudent(String fName, String sName, boolean gender, int photo);
+    }
+
+    public static final int NO_ACTION_STUDENT = -1;
+
+    private Context context;
+    private OnActionInRecyclerViewListener onActionInRecyclerViewListener;
+
+    private List<Student> studentList = new ArrayList<>();
+    private int actionStudent = NO_ACTION_STUDENT;
+
+    //TODO сделать разделители
+
+    public StudentsRecyclerAdapter(Context context){
+        this.context = context;
+        this.onActionInRecyclerViewListener = (OnActionInRecyclerViewListener) context;
+    }
+
+    public void saveStudent(Student newStudent){
+        if (actionStudent == NO_ACTION_STUDENT){
+            studentList.add(newStudent);
+            notifyItemInserted(studentList.size() - 1);
+            actionStudent = studentList.size() - 1;
+        } else {
+            Student student = studentList.get(actionStudent);
+            student.setFirstName(newStudent.getFirstName());
+            student.setSecondName(newStudent.getSecondName());
+            student.setMaleGender(newStudent.isMaleGender());
+            student.setPhoto(newStudent.getPhoto());
+            notifyItemChanged(actionStudent);
+        }
+    }
+
+    public void deleteStudent(){
+        if (actionStudent != NO_ACTION_STUDENT){
+            studentList.remove(actionStudent);
+            notifyItemRemoved(actionStudent);
+            actionStudent = NO_ACTION_STUDENT;
+            onActionInRecyclerViewListener.onClearFields();
+        } else {
+            onActionInRecyclerViewListener.onClearFields();
+        }
+    }
+
+    public void setActionStudent(int actionStudent) {
+        this.actionStudent = actionStudent;
+    }
 
     @NonNull
     @Override
@@ -41,6 +91,21 @@ public class StudentsRecyclerAdapter extends RecyclerView.Adapter<StudentsRecycl
             super(itemView);
             avatar = itemView.findViewById(R.id.recycler_item__image);
             name = itemView.findViewById(R.id.recycler_item__text);
+            //TODO графтческое выделение записи нажатой
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    actionStudent = getAdapterPosition();
+
+                    Student student = studentList.get(getAdapterPosition());
+                    onActionInRecyclerViewListener.onActionStudent(
+                            student.getFirstName(),
+                            student.getSecondName(),
+                            student.isMaleGender(),
+                            student.getPhoto()
+                    );
+                }
+            });
         }
 
         private void bind(Student student){
