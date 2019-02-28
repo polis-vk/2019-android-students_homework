382739dee062988a192b48f9c36df76f1a76c55a
prohladenn <valershik39@gmail.com>
2019-03-01 02:29:10 +0300

    Demo Logic

--- app/src/main/java/ru/ok/technopolis/students/MainActivity.java
+++ app/src/main/java/ru/ok/technopolis/students/MainActivity.java
@@ -2,6 +2,8 @@ package ru.ok.technopolis.students;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.main_recycler_view);
+        StudentAdapter studentAdapter = new StudentAdapter();
+        recyclerView.setAdapter(studentAdapter);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
     }
+
 
 }
---
+++ app/src/main/java/ru/ok/technopolis/students/StudentAdapter.java
@@ -0,0 +1,57 @@
+package ru.ok.technopolis.students;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+
+    private final ArrayList<Student> students;
+
+    public StudentAdapter() {
+        this.students = new ArrayList<>();
+        students.add(new Student("A", "B", false, 1));
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_main, viewGroup, false);
+        return new StudentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHolder viewHolder, int i) {
+        viewHolder.bind(students.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return students.size();
+    }
+
+    static final class StudentViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView nameTextView;
+        private final ImageView avatarImageView;
+
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.student_item_text);
+            avatarImageView = itemView.findViewById(R.id.student_item_avatar);
+        }
+
+        private void bind(@NonNull Student student) {
+            nameTextView.setText(student.getFirstName() + " " + student.getSecondName());
+            avatarImageView.setImageResource(student.getPhoto());
+        }
+
+    }
+
+}
