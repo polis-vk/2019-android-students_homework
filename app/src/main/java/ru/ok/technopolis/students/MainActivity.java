f10ec723401a5c65c7d2014235c08c2b615cc0bb
Loginov_Arkadiy <loginov3.aa@edu.spbstu.ru>
2019-03-05 17:30:02 +0300

    убрал лишние импорты

--- app/src/main/java/ru/ok/technopolis/students/MainActivity.java
+++ app/src/main/java/ru/ok/technopolis/students/MainActivity.java
@@ -2,7 +2,6 @@ package ru.ok.technopolis.students;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.graphics.Bitmap;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +9,6 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
--- app/src/main/java/ru/ok/technopolis/students/Student.java
+++ app/src/main/java/ru/ok/technopolis/students/Student.java
@@ -1,6 +1,5 @@
 package ru.ok.technopolis.students;
 
-import android.graphics.Bitmap;
 
 import java.io.Serializable;
 
