7b5c2d967522ef7d428ee44269a344d4d465e0e1
oxovu <vlada318@live.com>
2019-03-05 17:28:53 +0300

    active element highlights radiobutton instead checkboxes keyboard above the layout (can scroll it)

--- app/src/main/java/ru/ok/technopolis/students/StudentAdapter.java
+++ app/src/main/java/ru/ok/technopolis/students/StudentAdapter.java
@@ -2,6 +2,7 @@ package ru.ok.technopolis.students;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,6 +17,7 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
 
     private final List<Student> students;
     private final Listener onStudentClickListener;
+    private int focus = -1;
 
     public StudentAdapter(List<Student> students, Listener onStudentClickListener) {
         this.students = students;
@@ -26,7 +28,11 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
     @Override
     public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.student_item, viewGroup, false);
-        view.setOnClickListener(v -> onStudentClickListener.onStudentClick((Student) v.getTag()));
+        view.setOnClickListener(v -> {
+            focus = ((RecyclerView.LayoutParams) v.getLayoutParams()).getViewLayoutPosition();
+            notifyDataSetChanged();
+            onStudentClickListener.onStudentClick((Student) v.getTag());
+        });
         return new StudentViewHolder(view);
     }
 
@@ -35,6 +41,15 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
         Student student = students.get(i);
         viewHolder.bind(student);
         viewHolder.itemView.setTag(student);
+        if (focus == i) {
+            viewHolder.itemView.setBackgroundColor(Color.LTGRAY);
+        } else {
+            viewHolder.itemView.setBackgroundColor(Color.WHITE);
+        }
+    }
+
+    public void setFocus(int i) {
+        focus = i;
     }
 
     @Override
@@ -62,11 +77,10 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
             int heigth = (int) ((double) 100 / bitmap.getWidth() * bitmap.getHeight());
             photoImageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 100, heigth, true));
         }
-
     }
 
-    interface Listener {
-        void onStudentClick(Student student);
-    }
+}
 
+interface Listener {
+    void onStudentClick(Student student);
 }
